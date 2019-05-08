ce58086a924a627d8b798cfea3747abe4cd7e329
Wahudamon <afwa.bagas.1999@gmail.com>
2019-05-09 04:07:50 +0700

    fixing ukuran huruf di navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -2,11 +2,8 @@
   <nav>
       <div class="nav-wrapper green">
         <div class="container">
-          <router-link to="/" class="brand-logo">Ngatain Bagas Kuy!</router-link>
-          <router-link to="/about" class="right hide-on-med-and-down">Tentang ini</router-link>
-          <!-- <ul id="nav-mobile" class="right hide-on-med-and-down">
-              <li><a href="#">Tentang Ini</a></li>              
-          </ul>     -->
+          <router-link to="/" class="brand-logo" style="font-size: 14pt">Ngatain Bagas Kuy!</router-link>
+          <router-link to="/about" class="right hide-on-med-and-down">Tentang ini</router-link>          
         </div>
       </div>
     </nav>
