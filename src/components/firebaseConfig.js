9c2c7301714e99a5e6f12677c132873d9416da27
Afwa Bagas Wahuda <afwa.bagas.1999@gmail.com>
2019-05-13 14:19:37 +0700

    uhhh

--- src/components/firebaseConfig.js
+++
@@ -1,8 +0,0 @@
-export default { 
-  apiKey: "AIzaSyDNvOX98_jOkxltMyobJanexBgntDrmD1c",
-  authDomain: "secretoapp-c0d35.firebaseapp.com",
-  databaseURL: "https://secretoapp-c0d35.firebaseio.com",
-  projectId: "secretoapp-c0d35",
-  storageBucket: "secretoapp-c0d35.appspot.com",
-  messagingSenderId: "382986925874"
-}
